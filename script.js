fb023e7b2cc7c9bc23aa2535245cdf47fb076ffa
braiancalot <calot665@hotmail.com>
2022-03-17 12:06:41 -0300

    finalizando código

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Menu
 const menu = document.querySelector('.menu')
 const nav = document.querySelector('header nav')
 
@@ -7,3 +8,46 @@ function toggleMenu(){
     nav.classList.toggle('active')
     menu.classList.toggle('active')
 }
+
+//Scroll to Top
+const btnToTop = document.querySelector('.to-top')
+
+btnToTop.addEventListener('click', () => {
+    document.documentElement.scrollTop = 0;
+})
+
+// Scroll animate
+const target = document.querySelectorAll('[data-anime]')
+const animationClass = 'animate'
+
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+      const context = this;
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+function animeScroll(){
+    const windowTop = window.scrollY + ((window.innerHeight * 3)/4);
+    target.forEach( element => {
+        if(windowTop > element.offsetTop) {
+            element.classList.add(animationClass)
+        } else {
+            element.classList.remove(animationClass)
+        }
+    })
+}
+
+window.addEventListener('scroll', function() {
+    animeScroll()
+})
+
+animeScroll()
